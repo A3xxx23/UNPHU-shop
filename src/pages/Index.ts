bd3110b3d81c9e4bf36c1e61567b54e9d16fd55e
Angel Aquino <aa23-0272@unphu.edu.do>
2025-07-06 21:58:09 -0400

    delete files from pages directory

--- src/pages/Index.ts
+++ src/pages/Index.ts
@@ -11,7 +11,6 @@ export * from './OrderUserPage';
 export * from './Faq';
 export * from './Return';
 export * from './Shipping';
-export * from './ProfilePage';
 
 //admin dashboard
 
@@ -21,7 +20,6 @@ export * from './dashboard/DashboardProductSlugPage';
 export * from './dashboard/DashboardOrdersPage';
 export * from './dashboard/DahboardOrderPage';
 export * from './dashboard/DashboardSales';
-export * from './dashboard/DashboardCustomers';
 export * from './dashboard/DashboardCustomer';
 export * from './dashboard/DashboardOverviewPage';
 export * from './dashboard/DashboardProducts';
